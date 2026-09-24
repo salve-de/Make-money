24d1d9e9c777ece146731e59937f4f24070d83d0
sato <sato.business.0117@gmail.com>
2026-09-25 03:51:36 +0900

    Expose inspector stream through feature public API

--- src/features/company-inspector/index.ts
+++ src/features/company-inspector/index.ts
@@ -1,2 +1,4 @@
 export { CompanyInspectorPane } from './CompanyInspectorPane';
 export type { CompanyInspectorPaneProps,TabType } from './model/section-props';
+
+export { UniversalIntelligenceStream } from './ui/UniversalIntelligenceStream';
